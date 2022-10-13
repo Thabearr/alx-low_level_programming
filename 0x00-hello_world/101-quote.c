48665e167d57873a443fb6c3d8a0fa465261311b
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-13 19:59:29 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,5 +5,5 @@
  * Return: Always 1 (Success)
  */
 int main(void)
-{putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+{putchar("/" "and that piece of art is useful" - Dora Korpar, 2015-10-19");
 return (1); }
