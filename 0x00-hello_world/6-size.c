392d5087b382ad802824a06749a9aae1387f0b54
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-13 18:59:32 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,16 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 int intType;
 float floatType;
 double doubleType;
 char charType;
-// sizeof evaluates the size of a variable
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
-return 0;
+return (0); 
 }
 
