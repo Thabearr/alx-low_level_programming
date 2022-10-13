ef2c174f48085eafd631b934e0c908d406e4fd89
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-13 20:08:35 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,6 @@
  * Description: 'the program's description'
  * Return: Always 1 (Success)
  */
-int main(void)
+char main(void)
 {putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1); }
