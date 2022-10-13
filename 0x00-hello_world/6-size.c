f06b83d90009f05f0ba3d5173b3c91763dae117d
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-13 19:00:18 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
-return (0); 
+return (0);
 }
 
