0f50062d38639fc079daf42b4460c431a66eeaac
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-13 17:49:08 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
  * main - Entry point
  * Description: 'the program's description'
  * Return: Always 0 (Success)
- */int main(void)
+ */
+int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0); }
