b6253a4744ffc270b2e95b46592c0b13c9fa1d83
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-13 20:14:28 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'the program's description'
+ *
  * Return: Always 1 (Success)
  */
-char main(void)
 int main(void)
-; {putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1); }
+{char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str1, 59);
+return (1);
+}
