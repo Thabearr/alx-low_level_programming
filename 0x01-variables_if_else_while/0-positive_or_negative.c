3c19b6e1e4de4fc5eec28a7472a48b372491c1f1
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-15 00:40:37 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 ; if (n>0);  
-printf("is positive"); 
+printf("is positive\n"); 
 ; if (n=0);   
-printf("is zero"); 
+printf("is zero\n"); 
 ; if (n<0); 
-printf("is negative"); 
+printf("is negative\n"); 
 return (0);
 }
