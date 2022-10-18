8197fdca65bf0331c3819e2264cf4a2dd24731f9
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-19 00:38:11 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,10 @@
 #include "main.h"
 /**
 * Main - void
-*
 * Description: 'Prints _Putchar as a message'
-*
 * Return: Always 0 (success)
 */
-int main(void)
+int main(0)
 {
 printf("_putchar\n");
 return (0);
