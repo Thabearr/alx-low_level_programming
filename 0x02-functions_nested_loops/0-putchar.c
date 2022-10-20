163b9023fcf16e5b1c0cd89884f3119e52849bed
Thabearr <113853913+Thabearr@users.noreply.github.com>
2022-10-20 01:03:08 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
-* Main - void
-* Description: 'Prints _Putchar as a message'
-* Return: Always 0 (success)
-*/
-int main(void)
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
 {
-puts("_putchar\n");
-return (0);
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
